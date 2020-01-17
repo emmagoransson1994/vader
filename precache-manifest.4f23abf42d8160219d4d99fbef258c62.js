9b0342c94785970af2bc9a4f97077e47fdd22349
Emma Göransson <emmagoransson@Emmas-MacBook-Pro-2.local>
2020-01-17 17:14:15 +0100

    Updates

---
+++ precache-manifest.4f23abf42d8160219d4d99fbef258c62.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00e3f30d9b700c38884895e210f14e5",
+    "url": "/emmagoransson1994/vader.git/index.html"
+  },
+  {
+    "revision": "ee0866e64d497d6d2b38",
+    "url": "/emmagoransson1994/vader.git/static/css/2.fd695920.chunk.css"
+  },
+  {
+    "revision": "080a0d65d7873eef26d9",
+    "url": "/emmagoransson1994/vader.git/static/css/main.ee3d5d84.chunk.css"
+  },
+  {
+    "revision": "ee0866e64d497d6d2b38",
+    "url": "/emmagoransson1994/vader.git/static/js/2.07a31787.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/emmagoransson1994/vader.git/static/js/2.07a31787.chunk.js.LICENSE"
+  },
+  {
+    "revision": "080a0d65d7873eef26d9",
+    "url": "/emmagoransson1994/vader.git/static/js/main.133edc4f.chunk.js"
+  },
+  {
+    "revision": "0927f63ff76fbb65d224",
+    "url": "/emmagoransson1994/vader.git/static/js/runtime-main.f920eaf0.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/emmagoransson1994/vader.git/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/emmagoransson1994/vader.git/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/emmagoransson1994/vader.git/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/emmagoransson1994/vader.git/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/emmagoransson1994/vader.git/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
